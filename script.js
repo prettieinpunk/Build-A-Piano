e2a172e81490dc507bbed92a54ee80c41c223c76
prettieinpunk <160554112+prettieinpunk@users.noreply.github.com>
2025-02-10 11:04:43 -0500

    Editted script.js

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
+//vars
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
 const keys = document.querySelectorAll('.key');
 
+// Listeners //
 keys.forEach((key) => {
   key.addEventListener('click', () => playNote(key));
 });
 
+// Handlers //
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
+
+  // Reset the notes current time to zero 
+  noteAudio.currentTime = 0;
+
+  // Play the note
   noteAudio.play();
+
+  // Add a class, active, for styling purposes
+  key.classList.add('active');
+
+    //Listewn for when sound has ended and remove active class
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
 }
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+ 
+  if (e.repeat) {
+    return;
+  }
+  
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
